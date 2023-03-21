c183925ba0a27cfb2b08caf5c939f8f28cc5d098
jzzyy <passby158@gmail.com>
2023-03-21 14:57:20 +0900

    소켓 이용한 랜덤채팅

---
+++ src/chats/models/chattings.model.ts
@@ -0,0 +1,32 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+import { Schema, SchemaFactory, Prop } from '@nestjs/mongoose';
+import { SchemaOptions, Document, Types } from 'mongoose';
+import { Socket as SocketModel } from './sockets.model';
+
+const options: SchemaOptions = {
+  collection: 'chattings',
+  timestamps: true,
+};
+
+@Schema(options)
+export class Chatting extends Document {
+  @Prop({
+    type: {
+      _id: { type: Types.ObjectId, required: true, ref: 'sockets' },
+      id: { type: String },
+      username: { type: String, requried: true },
+    },
+  })
+  @IsNotEmpty()
+  @IsString()
+  user: SocketModel;
+
+  @Prop({
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  chat: string;
+}
+
+export const ChattingSchema = SchemaFactory.createForClass(Chatting);
